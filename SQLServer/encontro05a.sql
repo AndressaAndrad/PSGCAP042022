3c88ad13fea0d75b199991bcd034e1b664996746
AndressaAndrad <101049309+AndressaAndrad@users.noreply.github.com>
2022-04-08 14:23:27 -0400

    Create encontro05a.sql
    inclusão de dados SQL

---
+++ SQLServer/encontro05a.sql
@@ -0,0 +1,15 @@
+--SELECT * FROM Produto
+--GO
+
+
+INSERT INTO Produto(ID_Produto,Descricao_Produto,Preco_Produto) 
+VALUES 
+      (3,'Fanta Laranja lata 150ml', 1.50 ),
+	  (4,'Fanta Uva lata 150ml', 1.50 ),
+	  (5,'Sprite lata 150ml', 1.50 ),
+	  (6,'Pepsi lata 150ml', 1.50 ),
+	  (7,'Pespi Twist lata 150ml', 1.50 ),
+	  (8,'Tubaina Funada lata 150ml', 1.50 ),
+	  (9,'Guaraná Antártica lata 150ml', 1.50 ),
+	  (10,'Dolly lata 150ml', 1.50 )
+GO
