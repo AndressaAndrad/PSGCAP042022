c6931d905baa3bb0003e90aaf219cd99ec896a09
Andressa Andrade <101049309+AndressaAndrad@users.noreply.github.com>
2022-04-14 15:44:14 -0400

    Create encontro08.sql
    ALTER TABLE= CRIAR TABELA
    ADD=ADICIONAR TABELA
    DROP-APAGAR

---
+++ SQLServer/encontro08.sql
@@ -0,0 +1,39 @@
+--CREATE TABLE Colaborador (
+--	ID_Colaborador INT NOT NULL IDENTITY (1,1) PRIMARY KEY,
+--	Nome_Colaborador VARCHAR(MAX) NOT NULL
+--)
+------------------------------------+-------------------------------------------+--------------------------
+--ALTER TABLE Colaborador 
+--	ADD 
+--		Situacao BIT NULL DEFAULT 1,
+--		DataInclusao DATETIME NULL DEFAULT GETDATE()
+
+--GO
+---+---------------------------------+----------------------------------------+----------------------------------
+--ALTER TABLE Colaborador
+--	ADD 
+--		Cpf_Colaborador CHAR(14) NOT NULL,
+--		Sobrenome_Colaborador VARCHAR(MAX) NOT NULL,
+--		Endereco_Colaboradoor VARCHAR(MAX) NOT NULL,
+--		Telefone_Colaborador CHAR(14) NOT NULL,
+--		Email_Colaborador VARCHAR(MAX) NULL
+--GO
+------------------------------------+-------------------------------------------+--------------------------
+--ALTER TABLE Colaborador
+--	ALTER COLUMN 
+--		Telefone_Colaborador VARCHAR(MAX) NULL
+--GO
+------------------------------------+-------------------------------------------+--------------------------
+--ALTER TABLE Colaborador
+--	ADD
+--		Item_Produto VARCHAR(MAX) NOT NULL
+--GO
+------------------------------------+-------------------------------------------+--------------------------
+--ALTER TABLE Colaborador
+--		DROP COLUMN 
+--			Item_Produto
+--GO
+------------------------------------+-------------------------------------------+--------------------------
+
+--DROP TABLE Colaborador
+--GO
