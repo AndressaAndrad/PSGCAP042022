464deb6da889412afbe69384cf19453c84d47f21
Andressa Andrade <101049309+AndressaAndrad@users.noreply.github.com>
2022-04-26 14:35:21 -0400

    Update Stored Procedure.sql

--- SQLServer/Stored Procedure.sql
+++ SQLServer/Stored Procedure.sql
@@ -21,3 +21,29 @@ AND MONTH(Data_Admissao_Funcionario) = @MES
 AND DAY(Data_Admissao_Funcionario) = @DIA
 ORDER BY Data_Admissao_Funcionario ASC
 
+
+--CRIANDO (PROGRAMA DE ARMAZENAMENTO) STORED PROCEDURES-- TERCEIRO PASSO
+
+CREATE PROCEDURE SP_Buscar_Funcionario_Por_Data_Admissao
+ @ANO INT,
+ @MES INT,
+ @DIA INT
+AS
+BEGIN
+	SELECT 
+		Matricula_Funcionario, 
+		ID_FuncDadosEmpresa, 
+		Nome_Funcionario, 
+		Sobrenome_Funcionario, 
+		Data_Admissao_Funcionario, 
+		Ctps_Funcionario
+
+	FROM  Funcionario_Dados_Empresa
+	WHERE YEAR(Data_Admissao_Funcionario) = @ANO
+	AND MONTH(Data_Admissao_Funcionario) = @MES
+	AND DAY(Data_Admissao_Funcionario) = @DIA
+	ORDER BY Data_Admissao_Funcionario ASC  
+END;
+
+--PARA EXECUTAR
+EXEC SP_Buscar_Funcionario_Por_Data_Admissao  1985,3,10
