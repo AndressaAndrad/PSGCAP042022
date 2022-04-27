324f7a8ddaf5e588dbe0c1982f50cb8e93307cd7
Andressa Andrade <101049309+AndressaAndrad@users.noreply.github.com>
2022-04-27 17:00:41 -0400

    Update PROGRAMANDO COM DADOS NO DATABASE.sql

--- SQLServer/PROGRAMANDO COM DADOS NO DATABASE.sql
+++ SQLServer/PROGRAMANDO COM DADOS NO DATABASE.sql
@@ -12,3 +12,26 @@ FROM Funcionario_Temp -->nome da tabela dentro do database
 WHERE ID_Funcionario_Temp = @CHAVESELECIONADA
 
 GO
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--Para não dar tabela zerada.
+
+DECLARE @TOTALREGISTROS INT
+DECLARE @CHAVEINICIAL INT
+DECLARE @CHAVESELECIONADA INT
+DECLARE @QTDE INT
+
+SET @QTDE = 0
+WHILE (@QTDE = 0)
+BEGIN
+	SET @TOTALREGISTROS = (SELECT COUNT(*) FROM Funcionario_Temp)
+	SET @CHAVEINICIAL = (SELECT MIN(ID_Funcionario_Temp) FROM Funcionario_Temp)
+	SET @CHAVESELECIONADA = (SELECT FLOOR(RAND()*(@TOTALREGISTROS - @CHAVEINICIAL) + @CHAVEINICIAL))
+	SET @QTDE = (SELECT COUNT(*) FROM Funcionario_Temp WHERE ID_Funcionario_Temp = @CHAVESELECIONADA)
+END
+
+SELECT  *
+FROM Funcionario_Temp
+WHERE ID_Funcionario_Temp = @CHAVESELECIONADA
+
+GO
