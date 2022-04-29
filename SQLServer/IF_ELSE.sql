80b41aabc13e9e1acd4830ea0524be725a3a166d
Andressa Andrade <101049309+AndressaAndrad@users.noreply.github.com>
2022-04-29 14:49:34 -0400

    Update IF_ELSE.sql

--- SQLServer/IF_ELSE.sql
+++ SQLServer/IF_ELSE.sql
@@ -14,3 +14,26 @@ ELSE IF (@SITUACAO <> 'A') AND (@SITUACAO <> 'I')
 BEGIN
 	PRINT 'INFORME O CÓDIGO ADEQUADO'
 END
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+Fiz um create mas estava com o set e tive que fazer um alter
+ALTER PROCEDURE SP_TABELA_ATIVO_INATIVO
+@SITUACAO CHAR(1)
+AS
+BEGIN
+	IF (@SITUACAO = 'A')
+	BEGIN
+		SELECT * FROM Tabela WHERE (Situacao = 'A') ORDER BY Nome ASC
+	END
+	ELSE IF (@SITUACAO = 'I')
+	BEGIN
+		SELECT * FROM Tabela WHERE (Situacao = 'I') ORDER BY Nome ASC
+	END
+	ELSE IF (@SITUACAO <> 'A') AND (@SITUACAO <> 'I')
+	BEGIN
+		PRINT 'INFORME O CÓDIGO ADEQUADO'
+	END
+END
+GO
+
+--EXEC SP_TABELA_ATIVO_INATIVO 'O'
+--GO
